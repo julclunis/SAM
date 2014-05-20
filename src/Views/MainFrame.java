b3e7f6f3c63c54819ac037ea0c37dda0f3da61d4
sammy <sammy@>
2014-05-19 22:53:34 -0400

    h

--- src/Views/MainFrame.java
+++ src/Views/MainFrame.java
@@ -13,6 +13,11 @@ package Views;
 public class MainFrame extends javax.swing.JFrame {
 
     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
      * Creates new form MainFrame
      */
     public MainFrame() {
