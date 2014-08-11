ccebe2e824542a704f02f724d7e34d0ed55f28fd
sammysemantics <sammy.davidson@gmail.com>
2014-08-11 01:31:35 -0400

    commit

--- src/gui/SAOPCAC.java
+++ src/gui/SAOPCAC.java
@@ -20,6 +20,7 @@ import javax.swing.Box;
 @SuppressWarnings("serial")
 public class SAOPCAC extends JPanel {
 	private JTextField fileOrDirectoryLocationTextfiled;
+	private JTextPane resultsFromProcessingTextField;
 
 	/**
 	 * Create the panel.
@@ -32,15 +33,19 @@ public class SAOPCAC extends JPanel {
 		fileOrDirectoryLocationTextfiled.setName("");
 		add(fileOrDirectoryLocationTextfiled);
 		
-		JButton button_1 = new JButton();
-		button_1.setBounds(10, 172, 141, 23);
-		button_1.setText("Select File be analyzed");
-		add(button_1);
+		JButton selectFileForAnalysisButton = new JButton();
+		selectFileForAnalysisButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		selectFileForAnalysisButton.setBounds(10, 172, 141, 23);
+		selectFileForAnalysisButton.setText("Select File be analyzed");
+		add(selectFileForAnalysisButton);
 		
-		JButton button_2 = new JButton();
-		button_2.setBounds(10, 200, 141, 23);
-		button_2.setText("Preview Text");
-		add(button_2);
+		JButton previewTextContentButton = new JButton();
+		previewTextContentButton.setBounds(10, 200, 141, 23);
+		previewTextContentButton.setText("Preview Text");
+		add(previewTextContentButton);
 		
 		JLabel label = new JLabel();
 		label.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -48,28 +53,16 @@ public class SAOPCAC extends JPanel {
 		label.setText("By Single File");
 		add(label);
 		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(442, 16, 2, 0);
-		separator.setOrientation(SwingConstants.VERTICAL);
-		add(separator);
-		
 		JLabel label_1 = new JLabel();
 		label_1.setFont(new Font("Tahoma", Font.BOLD, 11));
 		label_1.setBounds(13, 224, 134, 14);
 		label_1.setText("By Batch Process");
 		add(label_1);
 		
-		JButton button_4 = new JButton();
-		button_4.setBounds(10, 266, 141, 23);
-		button_4.setText("List File and Directory");
-		add(button_4);
-		
-		JCheckBox checkBox = new JCheckBox();
-		checkBox.setBounds(157, 266, 135, 23);
-		checkBox.setText("Include Sub-directories");
-		checkBox.setSelected(true);
-		checkBox.setForeground(new Color(255, 0, 51));
-		add(checkBox);
+		JButton listFilesAndDirectoryButton = new JButton();
+		listFilesAndDirectoryButton.setBounds(10, 266, 141, 23);
+		listFilesAndDirectoryButton.setText("List File and Directory");
+		add(listFilesAndDirectoryButton);
 		
 		JLabel label_2 = new JLabel();
 		label_2.setBounds(257, 112, 183, 14);
@@ -82,10 +75,10 @@ public class SAOPCAC extends JPanel {
 		label_3.setFont(new Font("Tahoma", Font.BOLD, 36));
 		add(label_3);
 		
-		JButton button_3 = new JButton();
-		button_3.setBounds(10, 242, 141, 23);
-		button_3.setText("Select  Directory");
-		add(button_3);
+		JButton selectDirectoryForBatchButton = new JButton();
+		selectDirectoryForBatchButton.setBounds(10, 242, 141, 23);
+		selectDirectoryForBatchButton.setText("Select  Directory");
+		add(selectDirectoryForBatchButton);
 		
 		JLabel label_4 = new JLabel();
 		label_4.setBounds(382, 50, 58, 15);
@@ -93,13 +86,15 @@ public class SAOPCAC extends JPanel {
 		label_4.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		add(label_4);
 		
-		JTextPane resultsFromProcessingTextField = new JTextPane();
+		resultsFromProcessingTextField = new JTextPane();
 		resultsFromProcessingTextField.setBounds(10, 6, 340, 98);
 		add(resultsFromProcessingTextField);
-		
-		Component horizontalStrut = Box.createHorizontalStrut(20);
-		horizontalStrut.setBounds(257, 242, 105, -15);
-		add(horizontalStrut);
 
+	}
+	public JTextPane getResultsFromProcessingTextField() {
+		return resultsFromProcessingTextField;
+	}
+	public JTextField getFileOrDirectoryLocationTextfiled() {
+		return fileOrDirectoryLocationTextfiled;
 	}
 }
